409e1d02c50e58d6b8ba08b6a78a1de70023f205
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-11 19:46:47 -0300

    extras_out

--- mate_pipeline.py
+++ mate_pipeline.py
@@ -3803,10 +3803,11 @@ def main(entrada_override=None, spreadsheet_url_or_id=None):
 
         if itens:
             data2.append({"range": f"'{tab_name}'!B9:C{9 + len(itens) - 1}", "values": [[a, b] for a, b in itens]})
-            data2.append({
-                "range": f"'{tab_name}'!B{start_extra_row}:C{start_extra_row + len(extras_out) - 1}",
-                "values": extras_out
-            })
+
+        data2.append({
+            "range": f"'{tab_name}'!B{start_extra_row}:C{start_extra_row + len(extras_out) - 1}",
+            "values": extras_out
+        })
 
         ALVOS = (
             "REQUERIMENTOS DE COMISSÃO",
