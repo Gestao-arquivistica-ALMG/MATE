c144657e204ea8f59b3c0a94843cff6f43caf5c1
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-13 19:42:49 -0300

    r1

--- mate_pipeline.py
+++ mate_pipeline.py
@@ -2651,14 +2651,14 @@ def main(entrada_override=None, spreadsheet_url_or_id=None):
         reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r7, "endRowIndex": r7 + 1, "startColumnIndex": 0, "endColumnIndex": 2},
             "top": {"style": "DOTTED", "color": {"red": 0.8, "green": 0.0, "blue": 0.0}}}})
 
-        reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r, "endRowIndex": r8, "startColumnIndex": 1, "endColumnIndex": 2},
+        reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r1, "endRowIndex": r8, "startColumnIndex": 1, "endColumnIndex": 2},
             "right": {"style": "SOLID", "color": {"red": 0.0, "green": 0.0, "blue": 0.0}},
             "bottom": {"style": "SOLID_MEDIUM", "color": {"red": 0.0, "green": 0.0, "blue": 0.0}}}})
 
-        reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r, "endRowIndex": r8, "startColumnIndex": 4, "endColumnIndex": 5},
+        reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r1, "endRowIndex": r8, "startColumnIndex": 4, "endColumnIndex": 5},
             "left": {"style": "SOLID", "color": {"red": 0.0, "green": 0.0, "blue": 0.0}}}})
 
-        reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r, "endRowIndex": r8, "startColumnIndex": 17, "endColumnIndex": 18},
+        reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r1, "endRowIndex": r8, "startColumnIndex": 17, "endColumnIndex": 18},
             "left": {"style": "SOLID_MEDIUM", "color": {"red": 0.8, "green": 0.0, "blue": 0.0}}}})
 
         reqs.append({"updateBorders": {"range": {"sheetId": sheet_id, "startRowIndex": r8, "endRowIndex": r8 + 1, "startColumnIndex": 0, "endColumnIndex": 25},
