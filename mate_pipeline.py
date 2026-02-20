f92958ace99f9ce30810ae64fc4902e3ba6d0647
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-20 14:40:30 -0300

    8_off

--- mate_pipeline.py
+++ mate_pipeline.py
@@ -2049,10 +2049,6 @@ def main(entrada_override=None, spreadsheet_url_or_id=None):
                 reqs.append(_dv_req(3, r, LISTA_DROPDOWN_3))
                 reqs.append(_cf_req(3, r, bg_hex="#e6cff2", fg_hex="#5a3286", index=0))
 
-            if c == "DROPDOWN_8":
-                reqs.append(_dv_req(3, r, LISTA_DROPDOWN_8))
-                reqs.append(_cf_req(3, r, bg_hex="#f2e6cf", fg_hex="#865a32", index=0))
-                reqs.append(_cf_left_of_c_req(r, bg_hex="#f2e6cf", fg_hex="#865a32", index=0))
 
             elif c == "DROPDOWN_4":
                 reqs.append(_dv_req(2, r, LISTA_DROPDOWN_4))
