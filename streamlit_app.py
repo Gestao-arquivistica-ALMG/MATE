160a9a558701777853fbc8f4bdef17300602d019
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-23 09:38:54 -0300

    double

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="MATE", layout="centered")
 st.title("MATE.IA")
 
 entrada = st.text_input("Digite a data do Diário do Legislativo.\nEXEMPLOS:")
+entrada = st.text_input("Digite a data do Diário do Legislativo.\nEXEMPLOS:")
 
 col1, col2 = st.columns([1, 1])
 with col1:
