60b4f18447085aa12135bfb7e852c31d8a4ced43
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-24 16:14:13 -0300

    # "cabeçalho" feito com layout Streamlit (sem HTML clicável) c1, c2 = st.columns([2, 8], gap="small")
    with c1:
        # LOGO vira o botão do menu
        if st.button(
            "",
            key="btn_menu_toggle",
            use_container_width=True,
        ):
            st.session_state.menu_open = not st.session_state.menu_open
            st.rerun()
    
        # desenha o logo por cima do botão
        st.markdown(
            """
            <div style="
                margin-top:-42px;
                display:flex;
                align-items:center;
                justify-content:flex-start;
                pointer-events:none;
            ">
              <img src="https://www.almg.gov.br/system/modules/br.gov.almg.portal/resources/img/logo/logo.svg"
                   style="height:38px;">
            </div>
            """,
            unsafe_allow_html=True,
        )
    
    with c2:
        st.markdown(
            """
            <div style="display:flex; align-items:center; justify-content:flex-end; height:45px; gap:12px; font-size:24px;">
              <a href="https://silegis.almg.gov.br/silegismg/login/login.jsp#/processos" target="_blank" style="text-decoration:none;">🔍</a>
              <a href="https://intra.almg.gov.br/" target="_blank" style="text-decoration:none;">👤</a>
            </div>
            """,
            unsafe_allow_html=True,
        )

--- streamlit_app.py
+++ streamlit_app.py
@@ -211,7 +211,7 @@ with c1:
             pointer-events:none;
         ">
           <img src="https://www.almg.gov.br/system/modules/br.gov.almg.portal/resources/img/logo/logo.svg"
-               style="height:38px;">
+               style="height:45px;">
         </div>
         """,
         unsafe_allow_html=True,
