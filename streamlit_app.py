d6f401c238fb67f79d5f30cdd60a01f34f71ea01
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-24 12:47:32 -0300

    # --- BOTÃO MENU (logo abaixo do header) ---
    col_menu, col_space = st.columns([1,9])
    
    with col_menu:
        if st.button("☰", key="menu_btn"):
            st.session_state.menu_open = True
    
    with col_space:
        st.write("")

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,17 +191,6 @@ target="_blank" style="text-decoration:none;">
 </div>
 """,unsafe_allow_html=True)
 
-# --- BOTÃO MENU (logo abaixo do header) ---
-
-col_menu, col_space = st.columns([1,9])
-
-with col_menu:
-    if st.button("☰", key="menu_btn"):
-        st.session_state.menu_open = True
-
-with col_space:
-    st.write("")
-
 # ================= HEADER =================
 st.markdown(
     '<div class="title" style="font-size:24px; font-weight:1000; font-height:100;">GERÊNCIA DE GESTÃO ARQUIVÍSTICA</div>',
