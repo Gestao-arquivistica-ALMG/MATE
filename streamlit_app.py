dd9a9ff8da63711752317998a29aa570b9890326
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-24 15:58:08 -0300

    c1, c2 = st.columns([1, 1], gap="small")

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,7 +188,7 @@ if "menu_open" not in st.session_state:
     st.session_state.menu_open = False
 
 # "cabeçalho" feito com layout Streamlit (sem HTML clicável)
-c1, c2, c3 = st.columns([1, 1, 1], gap="small")
+c1, c2 = st.columns([1, 1], gap="small")
 
 with c1:
     # botão real (sem recarregar por navegação)
@@ -197,20 +197,6 @@ with c1:
         st.rerun()
 
 with c2:
-    st.markdown(
-        """
-        <div style="display:flex; align-items:center; justify-content:center; height:45px;">
-          <a href="https://www.almg.gov.br/" target="_blank"><img src="https://www.almg.gov.br/system/modules/br.gov.almg.portal/resources/img/logo/logo.svg" style="height:45px;"></a>
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
-
-with c3:
-        # botão real (sem recarregar por navegação)
-    if st.button("☰", key="btn_menu_toggle"):
-        st.session_state.menu_open = not st.session_state.menu_open
-        st.rerun()
     st.markdown(
         """
         <div style="display:flex; align-items:center; justify-content:flex-end; gap:10px; height:45px; font-size:26px;">
