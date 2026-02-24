999a83651811cc2744357185bb3bd584a2d7c140
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-24 11:52:36 -0300

    <div style='font-size:30px;color:#cc0000'>
    <a href="https://silegis.almg.gov.br/silegismg/login/login.jsp#/processos"
    target="_blank" style="text-decoration:none;">
    🔍
    </a>
    
    &nbsp;&nbsp;
    
    <a href="https://intra.almg.gov.br/"
    target="_blank" style="text-decoration:none;">
    👤
    </a>

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,15 +120,16 @@ border-radius:12px;
 
 <div style='font-size:35px;color:#cc0000'>☰</div>
 
-<div>
-<a href="https://www.almg.gov.br/" target="_blank">
-<img src="https://www.almg.gov.br/system/modules/br.gov.almg.portal/resources/img/logo/logo.svg"
-style="height:45px;">
-</a>
-</div>
-
 <div style='font-size:30px;color:#cc0000'>
-🔍 👤
+<a href="https://silegis.almg.gov.br/silegismg/login/login.jsp#/processos"
+target="_blank" style="text-decoration:none;">
+🔍
+</a>
+
+<a href="https://intra.almg.gov.br/"
+target="_blank" style="text-decoration:none;">
+👤
+</a>
 </div>
 
 </div>
