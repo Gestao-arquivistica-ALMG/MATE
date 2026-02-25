492e90850af832f91fe16403483886af9bf48bc7
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-25 13:14:22 -0300

    padding:2px 18px;

--- streamlit_app.py
+++ streamlit_app.py
@@ -236,7 +236,7 @@ div[data-testid="stHorizontalBlock"]{
   max-width:100%;
   margin:0 auto 0 auto;
   background:white;
-  padding:10px 18px;
+  padding:2px 18px;
   border-radius:12px;
 }
 button[kind="secondary"][data-testid="baseButton-secondary"]{
