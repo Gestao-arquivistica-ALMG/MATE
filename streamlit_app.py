85459ffc10260739774e4b817a909d4d6756118d
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-02-25 11:53:46 -0300

    c1, c2 = st.columns([1, 8], gap="small")

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,7 +188,7 @@ if "menu_open" not in st.session_state:
     st.session_state.menu_open = False
 
 # "cabeçalho" feito com layout Streamlit (sem HTML clicável)
-c1, c2 = st.columns([1, 20], gap="small")
+c1, c2 = st.columns([1, 8], gap="small")
 
 with c1:
     # LOGO vira o botão do menu
