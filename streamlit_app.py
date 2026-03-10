c0e56a0c8866db39cdf6bc9b74386d62687a76c5
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-10 15:59:19 -0300

    <img src="{pdf_icon}" style="height:16px; vertical-align:middle; position:relative; top:-10px;">

--- streamlit_app.py
+++ streamlit_app.py
@@ -434,13 +434,13 @@ if rodar:
             f'''
             <div style="margin:0 0 8px 0; display:flex; justify-content:flex-start;">
                 <a href="javascript:void(0)" id="downloadExecPdf" style="text-decoration:none;">
-                    <img src="{pdf_icon}" style="height:16px; vertical-align:middle; position:relative; top:-5px;">
+                    <img src="{pdf_icon}" style="height:16px; vertical-align:middle; position:relative; top:-10px;">
                 </a>
             </div>
 
             <div style="margin:0 0 8px 0; display:flex; justify-content:flex-start;">
                 <a href="{diario_leg_page}" target="_blank" rel="noopener noreferrer" style="text-decoration:none;">
-                    <img src="{pdf_icon}" style="height:16px; vertical-align:middle; position:relative; top:-5px;">
+                    <img src="{pdf_icon}" style="height:16px; vertical-align:middle; position:relative; top:-10px;">
                 </a>
             </div>
 
