1dc34d2da884968935a4e9f76c0377a6335d0626
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-10 14:49:58 -0300

    https://cdn-icons-png.flaticon.com/512/4949/4949024.png

--- streamlit_app.py
+++ streamlit_app.py
@@ -387,12 +387,12 @@ if rodar:
         margin-right: 6px;
     """
 
-    open_icon = "https://cdn-icons-png.freepik.com/512/12690/12690112.png"
+    open_icon = "https://cdn-icons-png.flaticon.com/512/4949/4949024.png"
     pdf_icon = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1280px-PDF_file_icon.svg.png"
 
     st.markdown(
         f'''
-        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:10px; color:#31333F;">
+        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:11px; color:#31333F;">
             <a href="{diario_exec_page}" target="_blank" rel="noopener noreferrer" style="text-decoration:none; color:#31333F;">
                 <img src="{open_icon}" style="height:16px; vertical-align:middle; margin-right:6px;">
                 Diário do Executivo
@@ -407,7 +407,7 @@ if rodar:
 
     st.markdown(
         f'''
-        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:10px; color:#31333F;">
+        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:11px; color:#31333F;">
             <a href="{diario_leg_page}" target="_blank" rel="noopener noreferrer" style="text-decoration:none; color:#31333F;">
                 <img src="{open_icon}" style="height:16px; vertical-align:middle; margin-right:6px;">
                 Diário do Legislativo
@@ -422,7 +422,7 @@ if rodar:
 
     st.markdown(
         f'''
-        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:10px; color:#31333F;">
+        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:11px; color:#31333F;">
             <a href="{reuniao_plenario}" target="_blank" rel="noopener noreferrer" style="text-decoration:none; color:#31333F;">
                 <img src="{open_icon}" style="height:16px; vertical-align:middle; margin-right:6px;">
                 Reuniões de Plenário
@@ -434,7 +434,7 @@ if rodar:
 
     st.markdown(
         f'''
-        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:10px; color:#31333F;">
+        <div style="margin:0 0 8px 0; font-family:'Montserrat',sans-serif; font-size:11px; color:#31333F;">
             <a href="{reuniao_comissoes}" target="_blank" rel="noopener noreferrer" style="text-decoration:none; color:#31333F;">
                 <img src="{open_icon}" style="height:16px; vertical-align:middle; margin-right:6px;">
                 Reuniões de Comissões
