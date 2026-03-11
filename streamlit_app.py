8f7dff3934bfc8ecb7d41de34869cf1d8d72e049
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-11 10:56:45 -0300

    for i in range(3):     status_text.write("Inicializando" + "." * (i + 1))     time.sleep(0.3)

--- streamlit_app.py
+++ streamlit_app.py
@@ -337,7 +337,9 @@ if limpar:
 if rodar:
     progress_bar = st.progress(0)
     status_text = st.empty()
-    status_text.write("Inicializando…")
+for i in range(3):
+    status_text.write("Inicializando" + "." * (i + 1))
+    time.sleep(0.3)
 
     entrada_clean = (entrada or "").strip()
 
