468218062d8a7d81e362041ccb91a914c93839c1
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-11 19:48:36 -0300

    drawer (menu) por cima do

--- streamlit_app.py
+++ streamlit_app.py
@@ -208,7 +208,6 @@ button[kind="secondary"][data-testid="baseButton-secondary"]{
 # ================= MENU (OVERLAY NO CORPO) =================
 if st.session_state.get("menu_open", False):
 
-    # 2) drawer (menu) por cima do overlay
     st.markdown("""
     <div id="almg_menu_drawer" style="
       display: block !important;
