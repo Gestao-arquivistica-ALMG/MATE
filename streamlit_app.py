03bca551d1ad71b0067ab1e28f11d59601f87ac1
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-16 17:10:07 -0300

    status_text.markdown(     f""" <div style="display:flex;justify-content:space-between;align-items:center; font-family:'Montserrat',sans-serif;font-size:13px;color:#31333F;width:100%;">
    <span>Concluído 100%</span>
    
    <a href="{url_com_aba}" target="_blank" rel="noopener noreferrer"
    style="text-decoration:none;color:#31333F;font-weight:400;
    position:relative; top:1px;">
    Abrir planilha
    </a>
    
    <span>{result['aba']}</span>
    
    </div>
    """,
        unsafe_allow_html=True
    )

--- streamlit_app.py
+++ streamlit_app.py
@@ -606,16 +606,15 @@ if rodar:
         status_text.markdown(
             f"""
         <div style="display:flex;justify-content:space-between;align-items:center;
-        font-family:'Montserrat',sans-serif;font-size:13px;color:#31333F;width:100%;line-height:1.6;">
+        font-family:'Montserrat',sans-serif;font-size:13px;color:#31333F;width:100%;">
 
         <span>Concluído 100%</span>
 
-        <span>
         <a href="{url_com_aba}" target="_blank" rel="noopener noreferrer"
-        style="text-decoration:none;color:#31333F;font-weight:400;">
+        style="text-decoration:none;color:#31333F;font-weight:400;
+        position:relative; top:1px;">
         Abrir planilha
         </a>
-        </span>
 
         <span>{result['aba']}</span>
 
