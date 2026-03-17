e46bc5d74368fb85a40072439776570c4b30127d
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-17 12:25:19 -0300

    <link rel="preconnect" href="https://fonts.googleapis.com">     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">

--- streamlit_app.py
+++ streamlit_app.py
@@ -380,6 +380,9 @@ if rodar:
     with c_links:
         components.html(
             f"""
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">
             <style>
             .link-line a {{
                 font-family:'Montserrat',sans-serif;
