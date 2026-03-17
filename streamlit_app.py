6e107a3520cdb3c801263d7694c81b6515081dba
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-17 12:47:46 -0300

    style="height:90px;width:200px;">

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,7 +167,7 @@ with c1:
             pointer-events:none;
         ">
           <img src="https://www.almg.gov.br/system/modules/br.gov.almg.portal/resources/img/logo/logo.svg"
-               style="height:45px;width:100px;">
+               style="height:90px;width:200px;">
         </div>
         """,
         unsafe_allow_html=True,
