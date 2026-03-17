992c8228c5c10d355125c41d90eccf6e6b9dee47
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-17 14:19:20 -0300

    c1, c2, c3 = st.columns([0.5, 6, 2], gap="small")

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,7 +144,7 @@ if "menu_open" not in st.session_state:
     st.session_state.menu_open = False
 
 # "cabeçalho" feito com layout Streamlit (sem HTML clicável)
-c1, c2, c3 = st.columns([1, 6, 2], gap="small")
+c1, c2, c3 = st.columns([0.5, 6, 2], gap="small")
 
 with c1:
     if st.button(
