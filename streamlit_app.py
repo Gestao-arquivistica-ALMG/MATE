bcc37e00924dcaff08c714bf0fc2dbf19c4f90a9
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-03-17 12:09:42 -0300

    <div style="display:flex;justify-content:space-between;align-items:center; font-family:'Montserrat',sans-serif;font-size:13px;color:#31333F; font-weight:500;width:100%;">

--- streamlit_app.py
+++ streamlit_app.py
@@ -380,7 +380,9 @@ if rodar:
     with c_links:
         components.html(
             f"""
-            <div style="justify-content:space-between;font-family:'Montserrat',sans-serif;font-size:13px;color:#31333F;width:100%;">
+            <div style="display:flex;justify-content:space-between;align-items:center;
+            font-family:'Montserrat',sans-serif;font-size:13px;color:#31333F;
+            font-weight:500;width:100%;">
 
                 <div style="margin:0 0 8px 0;">
                     <a href="javascript:void(0)" id="openExecTop" style="margin-right:6px;text-decoration:none;">
