a71ce8c99493a10750e254cb40497c30f8c3d683
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-04-07 15:35:48 -0300

    font-size: 8px !important;

--- streamlit_app.py
+++ streamlit_app.py
@@ -311,7 +311,7 @@ st.markdown("""
 <style>
 /* label do text_input */
 div[data-testid="stTextInput"] label {
-    font-size: 13px !important;
+    font-size: 8px !important;
     line-height: 1.1 !important;
 }
 </style>
