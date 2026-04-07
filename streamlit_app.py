7940bc2f116845a9fb42bc6f7d6e9ba9fb85884a
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-04-07 15:45:06 -0300

    st.markdown(             "<div style='font-size:13px; white-space:nowrap;'>Informe uma data de publicação válida</div>",             unsafe_allow_html=True         )

--- streamlit_app.py
+++ streamlit_app.py
@@ -312,15 +312,15 @@ with st.form("form_mate", clear_on_submit=False):
     col1, col2 = st.columns([9,4], vertical_alignment="bottom")
 
     with col1:
+        st.markdown(
+            "<div style='font-size:13px; white-space:nowrap;'>Informe uma data de publicação válida</div>",
+            unsafe_allow_html=True
+        )
+        
         entrada = st.text_input(
             "Informe uma data de publicação válida",
             placeholder="Ex.: 24/02/2026 ou dia...",
             label_visibility="collapsed"
-        )
-
-        st.markdown(
-            "<div style='font-size:13px; white-space:nowrap;'>Informe uma data de publicação válida</div>",
-            unsafe_allow_html=True
         )
 
     with col2:  
