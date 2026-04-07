21aa38e4313899a46d43535b9bfa6ef3f284540b
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-04-07 16:07:14 -0300

    max-width:100% !important;

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ div[data-testid="stTextInput"] > div{
 
 /* Conteúdo do form inteiro em uma coluna central (label+input+lista+botões) */
 div[data-testid="stForm"] > div{
-  max-width:100%;
+  max-width:320px;
   margin:0 auto;
 }
 
