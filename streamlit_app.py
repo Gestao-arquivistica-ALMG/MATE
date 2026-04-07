5c8891ac811e0d687595117cfda95ddbb7018df8
Gestao-arquivistica-ALMG <gestaoarquivistica.almg@gmail.com>
2026-04-07 15:56:08 -0300

    margin-left: 50px;

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,8 +72,7 @@ div[data-testid="stTextInput"] > div{
 /* Conteúdo do form inteiro em uma coluna central (label+input+lista+botões) */
 div[data-testid="stForm"] > div{
   max-width:320px;
-  margin:0;
-  margin-left: 50px;
+  margin:0 auto;
 }
 
 /* Conteúdo legível: alinhamento à esquerda */
